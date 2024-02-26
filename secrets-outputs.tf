b2a34a7cd56d6548a4a00e212bb3e9e208a8636e
Joe Ferris <jferris@thoughtbot.com>
2024-02-26 15:01:13 -0500

    Add developer managed secret mappings to outputs

--- secrets-outputs.tf
+++ secrets-outputs.tf
@@ -1,3 +1,14 @@
+output "developer_managed_secrets" {
+  description = "SecretsManager environment variables managed by developers"
+  value = [
+    for secret in values(module.developer_managed_secrets) :
+    {
+      name                  = secret.secret_name
+      environment_variables = secret.environment_variables
+    }
+  ]
+}
+
 output "secrets_manager_secrets" {
   description = "SecretsManager environment variables"
   value = [
